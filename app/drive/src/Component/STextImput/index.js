7a3de3048e609e6938d93c028abd0610a7a5ece7
Licethd <liceth.dfe@gmail.com>
2021-07-29 05:08:26 -0400

    aaa

--- app/drive/src/Component/STextImput/index.js
+++ app/drive/src/Component/STextImput/index.js
@@ -56,6 +56,14 @@ class STextImput extends Component {
             ...this.propiedades.style,
             borderColor: "#ff0000"
         }
+        if (!this.propsTemp.style) {
+            this.propsTemp.style = this.propiedades.style;
+        }
+        this.propiedades.style = {
+            ...this.propiedades.style,
+            borderColor: "#660000",
+            borderWidth:1,
+        }
         return false;
     }
     verify() {
